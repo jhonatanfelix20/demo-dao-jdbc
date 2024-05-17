b6fb59816ab9ba7c73e0a9f080bdd3d34c261bba
Jhonatan Felix <jhonatanvfelix@gmail.com>
2024-05-17 12:29:19 -0300

    Criado a classe vendedor e testado na classe principal

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import entities.Departamento;
+import entities.Vendedor;
 
 public class Program {
     public static void main(String[] args) {
         
     Departamento obj = new Departamento(1, "Livros");
     System.out.println(obj);
+
+    Vendedor vendedor = new Vendedor(21, "Maria", "maria@gmail.com", new Date(), 3000.0, obj);
+
+    System.out.println(vendedor);
 
     }
 }
